53b4dcafd7e930637c52b1ebf1c407cd4a84f29e
Tarik BOUDAA <tarikboudaa@yahoo.fr>
2021-04-23 23:12:11 +0200

    Solution du TP2

--- tp2_spring_mvc_ex1_bll/src/main/java/com/ensah/config/AppConfig.java
+++ tp2_spring_mvc_ex1_bll/src/main/java/com/ensah/config/AppConfig.java
@@ -8,7 +8,6 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
